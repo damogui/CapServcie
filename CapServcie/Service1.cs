48a21baf0c018722152ce13f73f688e27ee1404d
郭佳 <guo_jia_jia@hotmail.com>
2017-07-13 10:24:10 +0800

    添加项目文件。

---
+++ CapServcie/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapServcie
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        //开始
+        protected override void OnStart(string[] args)
+        {
+        }
+        //结束
+        protected override void OnStop()
+        {
+        }
+    }
+}
