cb929e53ef9dc378639e88af3be0f8dd31d5e7f1
郭佳 <guo_jia_jia@hotmail.com>
2017-07-13 11:18:08 +0800

    抓取服务

--- CapServcie/Service1.cs
+++ CapServcie/Service1.cs
@@ -16,10 +16,28 @@ namespace CapServcie
         {
             InitializeComponent();
         }
+       
+       
+
         //开始
         protected override void OnStart(string[] args)
         {
+
+            //执行的方法和多长时间执行一次
+            System.Threading.Timer timer2 = new System.Threading.Timer(Docap, null, 0, 1000 * 6);//6秒执行一次
         }
+
+
+        /// <summary>
+        /// 进行也去操作
+        /// </summary>
+        /// <param name="state"></param>
+        private static void Docap(object state)
+        {
+            throw new NotImplementedException();
+        }
+
+
         //结束
         protected override void OnStop()
         {
